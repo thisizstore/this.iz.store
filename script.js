23d6d91576e62a610f21b38017daa6dc2869ffdd
thisizstore <149288297+thisizstore@users.noreply.github.com>
2024-12-28 18:24:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+function plusSlides(n) {
+    showSlidesManual(slideIndex += n);
+}
+
+function showSlidesManual(n) {
+    let slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
